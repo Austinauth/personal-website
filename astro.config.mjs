55061469c27203bbc95fdd0e9485ea5ad183e327
Austin Auth <austinauth@microsoft.com>
2026-05-20 12:28:36 -0500

    Add GitHub Pages deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://austinauth.com',
+  site: 'https://austinauth.github.io',
+  base: '/personal-website',
   output: 'static',
   integrations: [sitemap()],
 });
